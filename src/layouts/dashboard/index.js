98fdcf3117b34fdf773cfaeabadc8f19b65da20c
Juniorawan06 <haseebawan.1560@gmail.com>
2024-06-21 03:56:05 +0500

    stats api added

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -22,69 +22,143 @@ import BuildByDevelopers from "layouts/dashboard/components/BuildByDevelopers";
 import WorkWithTheRockets from "layouts/dashboard/components/WorkWithTheRockets";
 import Projects from "layouts/dashboard/components/Projects";
 import OrderOverview from "layouts/dashboard/components/OrderOverview";
+import axios from "axios";
+import { useSoftUIController, setIsLoading } from '../../context';
 
 // Data
 import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
 import gradientLineChartData from "layouts/dashboard/data/gradientLineChartData";
+import { useEffect, useState } from "react";
 
 function Dashboard() {
   const { size } = typography;
   const { chart, items } = reportsBarChartData;
 
+  
+  const [controller, dispatch] = useSoftUIController();
+  const { isLoading } = controller;
+
+  const [dailyLocalwork, setDailyLocalwork] = useState(0);
+  const [dailyBatrips, setDailyBatrips] = useState(0);
+
+  const [weelyLocalwork, setWeeklyLocalwork] = useState(0);
+  const [weelyBatrips, setWeeklyBatrips] = useState(0);
+
+  const [yearlyLocalwork, setYearlyLocalwork] = useState(0);
+  const [yearlyBatrips, setYearlyBatrips] = useState(0);
+
+  const baseURL = `https://dynamic-unity-production.up.railway.app`
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(dispatch, true);
+      try {
+        const [localworkResponse, batripsResponse, weelyLocalworkResponse, weelyBatripsResponse, yearlyLocalworkResponse, yearlyBatripsResponse] = await Promise.all([
+          axios.get(`${baseURL}/daily_localwork`, {
+            headers: { 'Content-Type': 'application/json' },
+          }),
+          axios.get(`${baseURL}/daily_batrips`, {
+            headers: { 'Content-Type': 'application/json' },
+          }),
+          axios.get(`${baseURL}/weekly_localwork`, {
+            headers: { 'Content-Type': 'application/json' },
+          }),
+          axios.get(`${baseURL}/weekly_batrips`, {
+            headers: { 'Content-Type': 'application/json' },
+          }),
+          axios.get(`${baseURL}/yearly_localwork`, {
+            headers: { 'Content-Type': 'application/json' },
+          }),
+          axios.get(`${baseURL}/yearly_batrips`, {
+            headers: { 'Content-Type': 'application/json' },
+          }),
+        ]);
+
+        setDailyLocalwork(localworkResponse.data.data);
+        setDailyBatrips(batripsResponse.data.data);
+
+        setWeeklyLocalwork(weelyLocalworkResponse.data.data);
+        setWeeklyBatrips(weelyBatripsResponse.data.data);
+
+        setYearlyLocalwork(yearlyLocalworkResponse.data.data);
+        setYearlyBatrips(yearlyBatripsResponse.data.data);
+
+        setIsLoading(dispatch, false);
+      } catch (error) {
+        setIsLoading(dispatch, false);
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
   return (
     <DashboardLayout>
       <DashboardNavbar />
+
       <SoftBox py={3}>
         <SoftBox mb={3}>
+
+          <h2>Daily Report</h2>
           <Grid container spacing={3}>
-            <Grid item xs={12} sm={6} xl={3}>
+            <Grid item xs={12} sm={6} xl={6}>
               <MiniStatisticsCard
-                title={{ text: "today's money" }}
-                count="$53,000"
-                percentage={{ color: "success", text: "+55%" }}
-                icon={{ color: "info", component: "paid" }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} xl={3}>
-              <MiniStatisticsCard
-                title={{ text: "today's users" }}
-                count="2,300"
-                percentage={{ color: "success", text: "+3%" }}
+                title={{ text: "Local Works" }}
+                count={`Data Scraper Today: ${dailyLocalwork} Rows`} 
                 icon={{ color: "info", component: "public" }}
               />
             </Grid>
-            <Grid item xs={12} sm={6} xl={3}>
+            <Grid item xs={12} sm={6} xl={6}>
               <MiniStatisticsCard
-                title={{ text: "new clients" }}
-                count="+3,462"
-                percentage={{ color: "error", text: "-2%" }}
-                icon={{ color: "info", component: "emoji_events" }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} xl={3}>
-              <MiniStatisticsCard
-                title={{ text: "sales" }}
-                count="$103,430"
-                percentage={{ color: "success", text: "+5%" }}
-                icon={{
-                  color: "info",
-                  component: "shopping_cart",
-                }}
+                title={{ text: "BA Trips" }}
+                count={`Data Scraper Today: ${dailyBatrips} Rows`} 
+                icon={{ color: "info", component: "public" }}
               />
             </Grid>
           </Grid>
-        </SoftBox>
-        {/* <SoftBox mb={3}>
+
+          <br />
+          <h2>Weely Report</h2>
           <Grid container spacing={3}>
-            <Grid item xs={12} lg={7}>
-              <BuildByDevelopers />
+            <Grid item xs={12} sm={6} xl={6}>
+              <MiniStatisticsCard
+                title={{ text: "Local Works" }}
+                count={`Data Scraper this Month: ${weelyLocalwork} Rows`} 
+                icon={{ color: "info", component: "public" }}
+              />
             </Grid>
-            <Grid item xs={12} lg={5}>
-              <WorkWithTheRockets />
+            <Grid item xs={12} sm={6} xl={6}>
+              <MiniStatisticsCard
+                title={{ text: "BA Trips" }}
+                count={`Data Scraper this Month: ${weelyBatrips} Rows`} 
+                icon={{ color: "info", component: "public" }}
+              />
             </Grid>
           </Grid>
-        </SoftBox> */}
-        <SoftBox mb={3}>
+
+          <br />
+          <h2>Annual Report</h2>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} xl={6}>
+              <MiniStatisticsCard
+                title={{ text: "Local Works" }}
+                count={`Data Scraper this Year: ${yearlyLocalwork} Rows`} 
+                icon={{ color: "info", component: "public" }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} xl={6}>
+              <MiniStatisticsCard
+                title={{ text: "BA Trips" }}
+                count={`Data Scraper this Year: ${yearlyBatrips} Rows`} 
+                icon={{ color: "info", component: "public" }}
+              />
+            </Grid>
+          </Grid>
+
+        </SoftBox>
+
+        {/* <SoftBox mb={3}>
           <Grid container spacing={3}>
             <Grid item xs={12} lg={5}>
               <ReportsBarChart
@@ -119,7 +193,8 @@ function Dashboard() {
               />
             </Grid>
           </Grid>
-        </SoftBox>
+        </SoftBox> */}
+
         {/* <Grid container spacing={3}>
           <Grid item xs={12} md={6} lg={8}>
             <Projects />
